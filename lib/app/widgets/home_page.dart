2ee0e628fa7b47c29262a258ff0b35ca5cb1ca3b
Azrael <wuyanbin888888@163.com>
2019-06-03 20:54:59 +0800

    Format: homePage

--- lib/app/widgets/home_page.dart
+++ lib/app/widgets/home_page.dart
@@ -24,62 +24,133 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           ListItem(
-              value: 1, title: AppLocalizations.of(context).bottomNavigation),
+            value: 1,
+            title: AppLocalizations.of(context).bottomNavigation,
+          ),
           ListItem(
-              value: 2,
-              title: AppLocalizations.of(context).bottomNavigationWithDocker),
+            value: 2,
+            title: AppLocalizations.of(context).bottomNavigationWithDocker,
+          ),
           ListItem(
-              value: 3, title: AppLocalizations.of(context).routeAnimation),
-          ListItem(value: 4, title: AppLocalizations.of(context).frostedGlass),
+            value: 3,
+            title: AppLocalizations.of(context).routeAnimation,
+          ),
           ListItem(
-              value: 5, title: AppLocalizations.of(context).retainPageStatus),
-          ListItem(value: 6, title: AppLocalizations.of(context).searchBar),
+            value: 4,
+            title: AppLocalizations.of(context).frostedGlass,
+          ),
           ListItem(
-              value: 7,
-              title: AppLocalizations.of(context).wrapStreamingLayout),
+            value: 5,
+            title: AppLocalizations.of(context).retainPageStatus,
+          ),
           ListItem(
-              value: 8, title: AppLocalizations.of(context).expandClosedCase),
+            value: 6,
+            title: AppLocalizations.of(context).searchBar,
+          ),
           ListItem(
-              value: 9, title: AppLocalizations.of(context).bezierCurveCutting),
+            value: 7,
+            title: AppLocalizations.of(context).wrapStreamingLayout,
+          ),
           ListItem(
-              value: 10,
-              title: AppLocalizations.of(context).splashScreenAnimation),
+            value: 8,
+            title: AppLocalizations.of(context).expandClosedCase,
+          ),
           ListItem(
-              value: 11,
-              title: AppLocalizations.of(context).rightArrawReturnPreviousPage),
-          ListItem(value: 12, title: AppLocalizations.of(context).tooltip),
-          ListItem(value: 13, title: AppLocalizations.of(context).dragDrop),
+            value: 9,
+            title: AppLocalizations.of(context).bezierCurveCutting,
+          ),
           ListItem(
-              value: 14,
-              title: AppLocalizations.of(context).roundAvatarRoundedEffect),
-          ListItem(value: 15, title: AppLocalizations.of(context).gridList),
-          ListItem(value: 16, title: AppLocalizations.of(context).slidingOff),
-          ListItem(value: 17, title: AppLocalizations.of(context).animatedList),
-          ListItem(value: 18, title: AppLocalizations.of(context).fileIO),
-          ListItem(value: 19, title: AppLocalizations.of(context).netRequest),
+            value: 10,
+            title: AppLocalizations.of(context).splashScreenAnimation,
+          ),
           ListItem(
-              value: 20, title: AppLocalizations.of(context).platformChannel),
+            value: 11,
+            title: AppLocalizations.of(context).rightArrawReturnPreviousPage,
+          ),
           ListItem(
-              value: 21,
-              title: AppLocalizations.of(context).textFontStyleButton),
-          ListItem(value: 22, title: AppLocalizations.of(context).imageIcon),
+            value: 12,
+            title: AppLocalizations.of(context).tooltip,
+          ),
           ListItem(
-              value: 23,
-              title: AppLocalizations.of(context).radioSwitchCheckBox),
-          ListItem(value: 24, title: AppLocalizations.of(context).inputBoxForm),
-          ListItem(value: 25, title: AppLocalizations.of(context).layout),
+            value: 13,
+            title: AppLocalizations.of(context).dragDrop,
+          ),
           ListItem(
-              value: 26, title: AppLocalizations.of(context).containerWidget),
+            value: 14,
+            title: AppLocalizations.of(context).roundAvatarRoundedEffect,
+          ),
           ListItem(
-              value: 27, title: AppLocalizations.of(context).scrollableWidget),
+            value: 15,
+            title: AppLocalizations.of(context).gridList,
+          ),
           ListItem(
-              value: 28, title: AppLocalizations.of(context).functionalWidget),
+            value: 16,
+            title: AppLocalizations.of(context).slidingOff,
+          ),
           ListItem(
-              value: 29,
-              title: AppLocalizations.of(context).eventHandlingNotification),
-          ListItem(value: 30, title: AppLocalizations.of(context).customWidget),
-          ListItem(value: 31, title: AppLocalizations.of(context).useRedux),
-          ListItem(value: 32, title: AppLocalizations.of(context).intl),
+            value: 17,
+            title: AppLocalizations.of(context).animatedList,
+          ),
+          ListItem(
+            value: 18,
+            title: AppLocalizations.of(context).fileIO,
+          ),
+          ListItem(
+            value: 19,
+            title: AppLocalizations.of(context).netRequest,
+          ),
+          ListItem(
+            value: 20,
+            title: AppLocalizations.of(context).platformChannel,
+          ),
+          ListItem(
+            value: 21,
+            title: AppLocalizations.of(context).textFontStyleButton,
+          ),
+          ListItem(
+            value: 22,
+            title: AppLocalizations.of(context).imageIcon,
+          ),
+          ListItem(
+            value: 23,
+            title: AppLocalizations.of(context).radioSwitchCheckBox,
+          ),
+          ListItem(
+            value: 24,
+            title: AppLocalizations.of(context).inputBoxForm,
+          ),
+          ListItem(
+            value: 25,
+            title: AppLocalizations.of(context).layout,
+          ),
+          ListItem(
+            value: 26,
+            title: AppLocalizations.of(context).containerWidget,
+          ),
+          ListItem(
+            value: 27,
+            title: AppLocalizations.of(context).scrollableWidget,
+          ),
+          ListItem(
+            value: 28,
+            title: AppLocalizations.of(context).functionalWidget,
+          ),
+          ListItem(
+            value: 29,
+            title: AppLocalizations.of(context).eventHandlingNotification,
+          ),
+          ListItem(
+            value: 30,
+            title: AppLocalizations.of(context).customWidget,
+          ),
+          ListItem(
+            value: 31,
+            title: AppLocalizations.of(context).useRedux,
+          ),
+          ListItem(
+            value: 32,
+            title: AppLocalizations.of(context).intl,
+          ),
         ],
       ),
       drawer: AppDrawer(),
